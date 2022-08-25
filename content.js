3b9bff652ff55777b97a0fe3b43c6544b43b9a0a
heesangbb <heesangbb@gmail.com>
2022-08-25 14:10:34 +0900

    Added displays time in Udemy

--- content.js
+++ content.js
@@ -1,40 +1,116 @@
-const hrefType = {
-  playlist: {
-    time: '#content ytd-playlist-video-list-renderer ytd-thumbnail-overlay-time-status-renderer #text',
-    info: '#content ytd-playlist-sidebar-primary-info-renderer #stats',
-  },
-  watch: {
-    time: '#content ytd-playlist-panel-renderer ytd-thumbnail-overlay-time-status-renderer #text',
-    info: '#content ytd-playlist-panel-renderer #header-description',
-  },
-  video: {
-    time: '#content ytd-player .ytp-time-duration',
-    info: '#content ytd-player .ytp-time-display span:nth-child(2)',
-  },
+const run = () => {
+  youtubePlayListTime();
+  youtubeWatchListTime();
+  youtubeVideoTime();
+  udemyCourseTime();
+  udemyCourseSectionTime();
 };
 
-const run = () => {
-  let viewType;
-  const hrefPlaylist = window.location.href.includes('playlist');
-  if (hrefPlaylist) viewType = hrefType.playlist;
-  const hrefWatch = window.location.href.includes('watch');
-  if (hrefWatch) viewType = hrefType.watch;
-  if (!viewType) return;
+const youtubePlayListTime = () => {
+  const href = window.location.href;
+  const hrefCheck = href.includes('youtube.com') && href.includes('playlist');
+  if (!hrefCheck) return;
+  // console.log('youtube.com youtubePlayListTime');
 
-  // play list
-  const list = document.querySelectorAll(viewType.time);
+  const target = {
+    time: '#content ytd-playlist-video-list-renderer ytd-thumbnail-overlay-time-status-renderer #text',
+    info: '#content ytd-playlist-sidebar-primary-info-renderer #stats',
+  };
+
+  const list = document.querySelectorAll(target.time);
   if (list) {
     const total = calcTime(list);
     const content = `Playlist Duration: ${getHMSText(total)} ⏱`;
-    render(viewType.info, total, content, 'p');
+    render(target.info, total, content, 'p');
   }
+};
 
-  // video time
-  const videoTime = document.querySelectorAll(hrefType.video.time);
-  if (hrefWatch && videoTime) {
+const youtubeWatchListTime = () => {
+  const href = window.location.href;
+  const hrefCheck = href.includes('youtube.com') && href.includes('watch');
+  if (!hrefCheck) return;
+  // console.log('youtube.com youtubeWatchListTime');
+
+  const target = {
+    time: '#content ytd-playlist-panel-renderer ytd-thumbnail-overlay-time-status-renderer #text',
+    info: '#content ytd-playlist-panel-renderer #header-description',
+  };
+
+  const list = document.querySelectorAll(target.time);
+  if (list) {
+    const total = calcTime(list);
+    const content = `Playlist Duration: ${getHMSText(total)} ⏱`;
+    render(target.info, total, content, 'p');
+  }
+};
+
+const youtubeVideoTime = () => {
+  const href = window.location.href;
+  const hrefCheck = href.includes('youtube.com') && href.includes('watch');
+  if (!hrefCheck) return;
+  // console.log('youtube.com youtubeVideoTime');
+
+  const target = {
+    time: '#content ytd-player .ytp-time-duration',
+    info: '#content ytd-player .ytp-time-display span:nth-child(2)',
+  };
+
+  const videoTime = document.querySelectorAll(target.time);
+  if (videoTime) {
     const total = calcTime(videoTime);
     const content = ` ⏱`;
-    render(hrefType.video.info, total, content, 'span');
+    render(target.info, total, content, 'span');
+  }
+};
+
+const udemyCourseTime = () => {
+  const href = window.location.href;
+  const hrefCheck = href.includes('udemy.com') && href.includes('course');
+  if (!hrefCheck) return;
+  // console.log('udemy.com course udemyCourseTime');
+
+  const target =
+    '[data-purpose=dashboard-overview-container] [data-purpose=course-additional-stats] div:last-child';
+  const videoTime = document.querySelector(target);
+  if (videoTime) {
+    const time = parseFloat(videoTime.innerText.replace(/[^0-9\.]/g, ''));
+    const total = String(time * 60 * 60);
+    const content = ` ⏱`;
+    render(target.fo, total, content, 'span');
+  }
+};
+
+const udemyCourseSectionTime = () => {
+  const href = window.location.href;
+  const hrefCheck = href.includes('udemy.com') && href.includes('course');
+  if (!hrefCheck) return;
+  // console.log('udemy.com course udemyCourseSectionTime');
+
+  const target =
+    '[data-purpose=curriculum-section-container] [data-purpose=section-duration]';
+  const videoTimes = document.querySelectorAll(target);
+  if (videoTimes) {
+    for (let elem of videoTimes) {
+      const text = elem.innerText
+        .split('|')[1]
+        .replace(/[^0-9]/g, ' ')
+        .split(' ')
+        .filter(Boolean)
+        .reverse();
+
+      const [m = 0, h = 0] = text;
+      const total = String(h * 60 * 60 + m * 60);
+      const content = ` ⏱`;
+
+      let more = elem.querySelector('#playback-time');
+      if (!more) {
+        more = document.createElement('span');
+        more.setAttribute('id', 'playback-time');
+      }
+      more.textContent = content;
+      more.title = getPlaybackSpeed(total, [0.5, 0.75, 1.25, 1.5, 1.75, 2.0]);
+      elem.appendChild(more);
+    }
   }
 };
 
@@ -71,23 +147,24 @@ const getHMSText = (sec, speed = 1) => {
 const render = (info, total, content, moreTag) => {
   const infoDiv = document.querySelector(info);
 
-  let span = infoDiv.querySelector('#playback-time');
-  if (!span) {
-    span = document.createElement(moreTag);
-    span.setAttribute('id', 'playback-time');
-    more = document.createElement('span');
+  let more = infoDiv.querySelector('#playback-time');
+  if (!more) {
+    more = document.createElement(moreTag);
+    more.setAttribute('id', 'playback-time');
   }
-  span.textContent = content;
-  span.title = `[Playback speed]\n`;
-  span.title += `0.25x: ${getHMSText(total / 0.25)}\n`;
-  span.title += `0.50x: ${getHMSText(total / 0.5)}\n`;
-  span.title += `0.75x: ${getHMSText(total / 0.75)}\n`;
-  span.title += `1.25x: ${getHMSText(total / 1.25)}\n`;
-  span.title += `1.75x: ${getHMSText(total / 1.75)}\n`;
-  span.title += `2.00x: ${getHMSText(total / 2.0)}`;
+  more.textContent = content;
+  more.title = getPlaybackSpeed(total, [0.25, 0.5, 0.75, 1.25, 1.5, 1.75, 2.0]);
 
-  infoDiv.appendChild(span);
   infoDiv.appendChild(more);
+};
+
+const getPlaybackSpeed = (total, speedArr) => {
+  return (
+    `[Playback speed]\n` +
+    speedArr
+      .map((speed) => `${String(speed)}x: ${getHMSText(total / speed)}`)
+      .join(`\n`)
+  );
 };
 
 setInterval(run, 1500);
